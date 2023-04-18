cc471f2d8b06a2356e1a6246999d8f987be4902c
Adrian Echazabal <93956558+Itchibal@users.noreply.github.com>
2023-04-18 17:31:55 -0400

    MongoConnect Restored

---
+++ backend/MongoDBConnect.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+
+var config = require('./config');
+
+const uri = "mongodb+srv://" + config.db.user + ":" + config.db.pass +"@moviecluster.00ru6v3.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err)=>{
+    if (err)
+        console.log(err)
+    else
+        console.log("Connected Successfully")
+})
+module.exports = client;
