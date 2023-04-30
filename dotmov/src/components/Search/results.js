8ae79be783775eb7b9096a8f9bccfe3b81825993
Mahdia Rashid <mrashid2019@fau.edu>
2023-04-29 23:37:25 -0400

    edited results and add to fav button

--- dotmov/src/components/Search/results.js
+++ dotmov/src/components/Search/results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import placeholderImg from './placeholder.png';
+import MovieContainer from "../../components/MovieContainer";
 
 
 const Results = () => {
@@ -11,34 +12,18 @@ const Results = () => {
     <div>
       <h2 style={{padding:"15px"}}>Search Results:</h2>
       {movies.length ? (
-        <div className="moviesContainer">
-          <div className="moviesGrid">
-            {movies.map((movie) => (
-              <div styles={{margin: "3%"}} className="card text-center singleMovie mb-3">
-              <div style={{margin:"2%"}} key={movie.id}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                  alt={movie.title}
-                  className="card-img-top" 
-                  style={{height: "480px"}}
-                  onError={(e) => {
-                    e.target.onerror = null;
-                    e.target.src = placeholderImg;
-                  }}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{movie.title}</h5>
-                  <div
-        className={`rating ${movie.vote_average >= 5 ? "green" : "red"}`}
-      > Rating: {movie.vote_average}
-      </div>
-                </div>
-
-              </div>
-              </div>
-            ))}
-            </div>
-          </div>
+  <div className="moviesContainer">
+    <div className="moviesGrid">
+      {movies.map((movie) => (
+        <MovieContainer
+          title={movie.title}
+          poster_path={movie.poster_path}
+          vote_average={movie.vote_average}
+          id={movie.id}
+        />
+      ))}
+    </div>
+  </div>
         
       ) : (
         <div className="text-center mt-5 display-6">Sorry, no results found </div>
