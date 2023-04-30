382fbccc24f269bd492675ac1a4e180a1cfd7589
NPlanche <89564236+NPlanche@users.noreply.github.com>
2023-04-29 23:57:29 -0400

    added home to details page link

--- dotmov/src/components/MovieContainer.js
+++ dotmov/src/components/MovieContainer.js
@@ -1,43 +1,60 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
-import { auth } from '../configs/firebase.js'
+import { auth } from "../configs/firebase.js";
+import Details from "../pages/details.js";
 
+import { Link } from "react-router-dom";
 
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
 
-const API_IMG ="https://image.tmdb.org/t/p/w500/";
-
-const MovieContainer = ({title, poster_path,vote_average, release_date, overview , id}) => {
-
+const MovieContainer = ({
+  title,
+  poster_path,
+  vote_average,
+  release_date,
+  overview,
+  id,
+}) => {
   const sendFavorite = (event) => {
     const userID = auth.currentUser.uid;
     const movieID = event.target.id;
-    axios.post('http://localhost:5678/AddFavorite', { userID, movieID })
-      .then(response => {
+    axios
+      .post("http://localhost:5678/AddFavorite", { userID, movieID })
+      .then((response) => {
         console.log(response.data);
         //axios.put()
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-  }
-return(
+  };
+  return (
     <div className="card text-center singleMovie mb-3">
+      <div className="card-body">
+        <Link to={`/details/${id}`}>
+          <img className="card-img-top" src={API_IMG + poster_path} />
+        </Link>
         <div className="card-body">
-            <img className= "card-img-top" src={API_IMG+poster_path} />
-            <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <div
-        className={`rating ${vote_average >= 5 ? "green" : "red"}`}
-      > Rating: {vote_average}
-      </div>
-      <div style={{marginTop:"10px"}}>
-      <button type="button" className="btn btn-dark" id={id} onClick={sendFavorite}> Add to favorites </button>
-      </div>
-            </div>
+          <h5 className="card-title">{title}</h5>
+          <div className={`rating ${vote_average >= 5 ? "green" : "red"}`}>
+            {" "}
+            Rating: {vote_average}
+          </div>
+          <div style={{ marginTop: "10px" }}>
+            <button
+              type="button"
+              className="btn btn-dark"
+              id={id}
+              onClick={sendFavorite}
+            >
+              {" "}
+              Add to favorites{" "}
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-)
-}
+  );
+};
 
 export default MovieContainer;
-  
