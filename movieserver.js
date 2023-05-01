a725f25fc018f829725da19c41709d8d6d4421b0
Adrian Echazabal <93956558+Itchibal@users.noreply.github.com>
2023-05-01 04:02:28 -0400

    Hosting

--- movieserver.js
+++ movieserver.js
@@ -16,8 +16,9 @@ app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-//app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('public'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
