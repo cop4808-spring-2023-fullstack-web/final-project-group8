7bda7fb3ff277fbacf1ab79e191788fd7f0f0b22
Adrian Echazabal <93956558+Itchibal@users.noreply.github.com>
2023-05-01 04:16:29 -0400

    Backed UpBefore Hosting Attemtp

--- backend/movieserver.js
+++ backend/movieserver.js
@@ -16,11 +16,11 @@ app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//});
 
 const MongoClient = require('mongodb').MongoClient;
 
